51470e6507404c8e3ffd2855d6fedd4169614022
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2024-05-18 11:01:34 +0900

    fix aboutme

--- src/app/components/AboutMe/page.tsx
+++ src/app/components/AboutMe/page.tsx
@@ -14,25 +14,22 @@ const AboutMe: React.FC = () => {
         <ul style={{ paddingLeft: 20 }}>
           <li>
             <Typography variant="subtitle1">
-              Application Engineer integrating API with Salesforce and AWS.
+              Application Engineer specializing in API integration with Salesforce and AWS, and enhancing backend APIs for enterprise systems.
             </Typography>
           </li>
           <li>
             <Typography variant="subtitle1">
-              Master&rsquo;s in Engineering, focused on computational social
-              science and machine learning.
+              Master&rsquo;s in Engineering, focused on enhancing user experiences in audio media through natural language processing and social media data analysis. My research improved radio listening experiences by optimizing playback speed using Twitter(X) data to enhance user satisfaction during limited listening times.
             </Typography>
           </li>
           <li>
             <Typography variant="subtitle1">
-              AWS-certified: Solutions Architect Professional and Security
-              Specialty.
+              AWS-certified: Solutions Architect Professional, Security Specialty, and four additional certifications.
             </Typography>
           </li>
           <li>
             <Typography variant="subtitle1">
-              Aspiring to blend my passion for solving real-world problems with
-              a keen interest in enhancing SaaS products🔥
+              Aspiring to blend my passion for solving real-world problems with a keen interest in enhancing software products. 🔥
             </Typography>
           </li>
         </ul>
