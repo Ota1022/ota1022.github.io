587a887013d3af02692cad65ae6472d87d748919
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2024-05-18 12:02:55 +0900

    fix certifications

--- src/app/components/Certifications/page.tsx
+++ src/app/components/Certifications/page.tsx
@@ -10,11 +10,20 @@ const Certifications: React.FC = () => {
       <Typography variant="h4" component="h3" sx={{ marginBottom: 2 }}>
         Certifications
       </Typography>
-      <Typography
-        variant="body1"
-        component="div"
-        sx={{ marginLeft: 2 }}
-      ></Typography>
+      <Typography variant="body1" component="div" sx={{ marginLeft: 2 }}>
+        <ul>
+          <li>TOEIC 865 points</li>
+          <li>IPA Fundamental Information Technology Engineer</li>
+          <li>IPA Applied Information Technology Engineer Examination</li>
+          <li>AWS Certified Cloud Practitioner (CLF-C01)</li>
+          <li>AWS Certified Solutions Architect - Associate (SAA-C03)</li>
+          <li>AWS Certified Developer - Associate (DVA-C02)</li>
+          <li>AWS Certified SysOps Administrator - Associate (SOA-C02)</li>
+          <li>AWS Certified Solutions Architect - Professional (SAP-C02)</li>
+          <li>AWS Certified Data Analytics - Specialty (DAS-C01)</li>
+          <li>AWS Certified Security - Specialty (SCS-C02)</li>
+        </ul>
+      </Typography>
     </Paper>
   );
 };
