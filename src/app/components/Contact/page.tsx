c6a2e65f4e862d65641b6ac9e0cd1317949eb5ec
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2024-07-28 13:54:38 +0900

    add zenn icon

--- src/app/components/Contact/page.tsx
+++ src/app/components/Contact/page.tsx
@@ -2,11 +2,18 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import XIcon from "@mui/icons-material/X";
-import { Box } from "@mui/material";
+import { Box, SvgIcon } from "@mui/material";
 import Link from "@mui/material/Link";
 import React from "react";
 
-const Contact: React.FC = () => {
+const ZennIcon: React.FC = (props) => (
+  <SvgIcon {...props} viewBox="0 0 24 24">
+    <title>Zenn</title>
+    <path d="M.264 23.771h4.984c.264 0 .498-.147.645-.352L19.614.874c.176-.293-.029-.645-.381-.645h-4.72c-.235 0-.44.117-.557.323L.03 23.361c-.088.176.029.41.234.41zM17.445 23.419l6.479-10.408c.205-.323-.029-.733-.41-.733h-4.691c-.176 0-.352.088-.44.235l-6.655 10.643c-.176.264.029.616.352.616h4.779c.234-.001.468-.118.586-.353z"/>
+  </SvgIcon>
+);
+
+const Contact: React.FC = (): JSX.Element => {
   return (
     <Box
       sx={{
@@ -17,19 +24,30 @@ const Contact: React.FC = () => {
       }}
     >
       <Link href="https://linkedin.com/in/itaru-ota-naist" color="inherit">
-        <LinkedInIcon sx={{ fontSize: 40 }} />
+        <Box sx={{ fontSize: 40 }}>
+          <LinkedInIcon />
+        </Box>
       </Link>
       <Link href="https://github.com/Ota1022" color="inherit">
-        <GitHubIcon sx={{ fontSize: 40 }} />
+        <Box sx={{ fontSize: 40 }}>
+          <GitHubIcon />
+        </Box>
       </Link>
       <Link href="https://x.com/iorandd" color="inherit">
-        <XIcon sx={{ fontSize: 40 }} />
+        <Box sx={{ fontSize: 40 }}>
+          <XIcon />
+        </Box>
       </Link>
       <Link href="https://speakerdeck.com/ota1022" color="inherit">
-        <DescriptionIcon sx={{ fontSize: 40 }} />
+        <Box sx={{ fontSize: 40 }}>
+          <DescriptionIcon />
+        </Box>
       </Link>
-      {/* Add other icons for SpeakerDeck, Zenn, and CV PDF with a similar pattern */}
-      {/* For custom icons, use <SvgIcon> with your SVG path and adjust the size similarly */}
+      <Link href="https://zenn.dev/iorandd" color="inherit">
+        <Box sx={{ fontSize: 40 }}>
+          <ZennIcon />
+        </Box>
+      </Link>
     </Box>
   );
 };
