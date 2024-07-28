635ff628e571854405e449d6828902d9fa3a3161
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2024-07-28 14:05:55 +0900

    fix speakerdeck icon and fix fontsize to 150

--- src/app/components/Contact/page.tsx
+++ src/app/components/Contact/page.tsx
@@ -1,4 +1,3 @@
-import DescriptionIcon from "@mui/icons-material/Description";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import XIcon from "@mui/icons-material/X";
@@ -13,6 +12,13 @@ const ZennIcon: React.FC = (props) => (
   </SvgIcon>
 );
 
+const SpeakerDeckIcon: React.FC = (props) => (
+  <SvgIcon {...props} viewBox="0 0 24 24">
+    <title>Speaker Deck</title>
+    <path d="M10.025 13.875H4.687a4.688 4.688 0 0 1 0-9.375h6.227a1.875 1.875 0 0 1 0 3.75H4.592a.937.937 0 1 0 0 1.875h5.337a4.687 4.687 0 1 1 0 9.375H1.875a1.875 1.875 0 0 1 0-3.75h8.15a.938.938 0 0 0 0-1.875zM13.97 19.5a5.635 5.635 0 0 0 2.396-3.75h3.026a.93.93 0 0 0 .921-.938V9.189a.93.93 0 0 0-.921-.938h-5.497c.438-.498.704-1.155.704-1.875s-.266-1.377-.704-1.875h6.418C22.35 4.5 24 6.179 24 8.25v7.5c0 2.071-1.65 3.75-3.687 3.75H13.97z"/>
+  </SvgIcon>
+);
+
 const Contact: React.FC = (): JSX.Element => {
   return (
     <Box
@@ -24,27 +30,27 @@ const Contact: React.FC = (): JSX.Element => {
       }}
     >
       <Link href="https://linkedin.com/in/itaru-ota-naist" color="inherit">
-        <Box sx={{ fontSize: 60 }}>
+        <Box sx={{ fontSize: 150 }}>
           <LinkedInIcon />
         </Box>
       </Link>
       <Link href="https://github.com/Ota1022" color="inherit">
-        <Box sx={{ fontSize: 60 }}>
+        <Box sx={{ fontSize: 150 }}>
           <GitHubIcon />
         </Box>
       </Link>
       <Link href="https://x.com/iorandd" color="inherit">
-        <Box sx={{ fontSize: 60 }}>
+        <Box sx={{ fontSize: 150 }}>
           <XIcon />
         </Box>
       </Link>
       <Link href="https://speakerdeck.com/ota1022" color="inherit">
-        <Box sx={{ fontSize: 60 }}>
-          <DescriptionIcon />
+        <Box sx={{ fontSize: 150 }}>
+          <SpeakerDeckIcon />
         </Box>
       </Link>
       <Link href="https://zenn.dev/iorandd" color="inherit">
-        <Box sx={{ fontSize: 60 }}>
+        <Box sx={{ fontSize: 150 }}>
           <ZennIcon />
         </Box>
       </Link>
