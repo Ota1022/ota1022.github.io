060a28e8bda43e1e72f5c0fc63de37ffd29ed48f
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2025-02-10 23:26:37 +0900

    fix experience

--- src/app/components/AboutMe/page.tsx
+++ src/app/components/AboutMe/page.tsx
@@ -14,30 +14,20 @@ const AboutMe: React.FC = () => {
         <ul style={{ paddingLeft: 20 }}>
           <li>
             <Typography variant="subtitle1">
-              Application Engineer specializing in API integration with
-              Salesforce and AWS, and enhancing backend APIs for enterprise
-              systems.
+              A Software Engineer with 3+ years of experience in developing.
             </Typography>
           </li>
           <li>
             <Typography variant="subtitle1">
               Master&rsquo;s in Engineering, focused on enhancing user
               experiences in audio media through natural language processing and
-              social media data analysis. My research improved radio listening
-              experiences by optimizing playback speed using Twitter(X) data to
-              enhance user satisfaction during limited listening times.
+              social media data analysis.
             </Typography>
           </li>
           <li>
             <Typography variant="subtitle1">
               AWS-certified: Solutions Architect Professional, Security
-              Specialty, and four additional certifications.
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="subtitle1">
-              Aspiring to blend my passion for solving real-world problems with
-              a keen interest in enhancing software products. 🔥
+              Specialty, etc.
             </Typography>
           </li>
         </ul>
