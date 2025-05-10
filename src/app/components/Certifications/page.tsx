30db15710d96db1d2cbecaafb81b113967854a13
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2025-05-10 13:21:24 +0900

    update certifications

--- src/app/components/Certifications/page.tsx
+++ src/app/components/Certifications/page.tsx
@@ -1,4 +1,4 @@
-import { Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
 
 const Certifications: React.FC = () => {
@@ -10,24 +10,51 @@ const Certifications: React.FC = () => {
       <Typography variant="h4" component="h3" sx={{ marginBottom: 2 }}>
         Certifications
       </Typography>
-      <Typography variant="body1" component="div" sx={{ marginLeft: 2 }}>
-        <ul>
-          <li>TOEIC 865 points</li>
-          <li>IPA Fundamental Information Technology Engineer</li>
-          <li>IPA Applied Information Technology Engineer</li>
-          <li>AWS Certified Cloud Practitioner (CLF-C01)</li>
-          <li>AWS Certified AI Practitioner (AIF-C01)</li>
-          <li>AWS Certified Solutions Architect - Associate (SAA-C03)</li>
-          <li>AWS Certified Developer - Associate (DVA-C02)</li>
-          <li>AWS Certified SysOps Administrator - Associate (SOA-C02)</li>
-          <li>AWS Certified Data Engineer - Associate (DEA-C01)</li>
-          <li>AWS Certified Machine Learning Engineer - Associate (MLA-C01)</li>
-          <li>AWS Certified Solutions Architect - Professional (SAP-C02)</li>
-          <li>AWS Certified Security - Specialty (SCS-C02)</li>
-          <li>AWS Certified Data Analytics - Specialty (DAS-C01)</li>
-          <li>AWS Certified Machine Learning - Specialty (MLS-C01)</li>
-        </ul>
-      </Typography>
+
+      <Box sx={{ mb: 3 }}>
+        <Typography variant="h6" component="h4" sx={{ mb: 1 }}>
+          AWS Certifications
+        </Typography>
+        <Typography variant="body1" component="div" sx={{ marginLeft: 2 }}>
+          <ul>
+            <li>AWS Certified Cloud Practitioner (CLF-C01)</li>
+            <li>AWS Certified AI Practitioner (AIF-C01)</li>
+            <li>AWS Certified Solutions Architect - Associate (SAA-C03)</li>
+            <li>AWS Certified Developer - Associate (DVA-C02)</li>
+            <li>AWS Certified SysOps Administrator - Associate (SOA-C02)</li>
+            <li>AWS Certified Data Engineer - Associate (DEA-C01)</li>
+            <li>AWS Certified Machine Learning Engineer - Associate (MLA-C01)</li>
+            <li>AWS Certified Solutions Architect - Professional (SAP-C02)</li>
+            <li>AWS Certified Security - Specialty (SCS-C02)</li>
+            <li>AWS Certified Data Analytics - Specialty (DAS-C01)</li>
+            <li>AWS Certified Machine Learning - Specialty (MLS-C01)</li>
+          </ul>
+        </Typography>
+      </Box>
+
+      <Box sx={{ mb: 3 }}>
+        <Typography variant="h6" component="h4" sx={{ mb: 1 }}>
+          Google Cloud Certifications
+        </Typography>
+        <Typography variant="body1" component="div" sx={{ marginLeft: 2 }}>
+          <ul>
+            <li>Google Cloud Associate Cloud Engineer</li>
+          </ul>
+        </Typography>
+      </Box>
+
+      <Box sx={{ mb: 3 }}>
+        <Typography variant="h6" component="h4" sx={{ mb: 1 }}>
+          Others
+        </Typography>
+        <Typography variant="body1" component="div" sx={{ marginLeft: 2 }}>
+          <ul>
+            <li>TOEIC 865 points</li>
+            <li>IPA Fundamental Information Technology Engineer</li>
+            <li>IPA Applied Information Technology Engineer</li>
+          </ul>
+        </Typography>
+      </Box>
     </Paper>
   );
 };
