241e0f91d6a5c23dc9dd5b123c724eaa675323ca
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2026-07-05 21:13:08 +0900

    Polish English portfolio content

--- src/app/components/Publication/page.tsx
+++ src/app/components/Publication/page.tsx
@@ -8,23 +8,24 @@ const Publication: FC = () => {
       elevation={3}
     >
       <Typography variant="h4" component="h3" sx={{ marginBottom: 2 }}>
-        Publication
+        Publications
       </Typography>
       <Typography variant="body1" component="div" sx={{ marginLeft: 2 }}>
         <ul>
           <li>
-            Improving the Experience of Listening Broadcast Programs Using
-            Social Media Data (Master&rsquo;s thesis){' '}
+            Improving the Listening Experience for Broadcast Programs Using
+            Social Media Data (Japanese), Master&rsquo;s thesis{' '}
             <Link
               href="https://library.naist.jp/opac/en/book/106760"
               target="_blank"
               rel="noopener"
             >
-              [PDF]
+              [Record]
             </Link>
           </li>
           <li>
-            Analysis of the Uniformity Rate of Tweets for Broadcast Programs,
+            Analysis of the Uniformity Rate of Tweets for Broadcast Programs
+            (Japanese),
             The 15th Forum on Data Engineering and Information Management
             (DEIM2023), 4a-3-2, 2023 (2023/3/5, Gifu, Japan){' '}
             <Link
@@ -36,8 +37,9 @@ const Publication: FC = () => {
             </Link>
           </li>
           <li>
-            Proposal of the Radio Programs Compression Method Using Twitter, The
-            14th Forum on Data Engineering and Information Management
+            A Proposal for a Radio Program Compression Method Using Twitter
+            (Japanese), The 14th Forum on Data Engineering and Information
+            Management
             (DEIM2022), C21-2, 2022 (2022/02/28, Online){' '}
             <Link
               href="https://proceedings-of-deim.github.io/DEIM2022/papers/C21-2.pdf"
